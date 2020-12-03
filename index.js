7adfdc21dd58a39169d3d591d3fe140169274b9c
Larry Gamboa <techmedia.lg@gmail.com>
2020-12-02 18:10:57 -0800

    create variables

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateMarkdown = require("./utils/generateMarkdown");
+
 // array of questions for user
 const questions = [
-
+    
 ];
 
 // function to write README file
